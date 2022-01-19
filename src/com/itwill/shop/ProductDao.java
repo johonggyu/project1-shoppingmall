c6a5c28662cccbea9df1b9b08eb639eb203529e3
hong <hk4592@naver.com>
2022-01-19 13:55:22 +0900

    네번째커밋

--- src/com/itwill/shop/ProductDao.java
+++ src/com/itwill/shop/ProductDao.java
@@ -1,7 +1,5 @@
 package com.itwill.shop;
 
 public class ProductDao {
-	public void insert() {
-		
-	}
+	
 }
--- src/com/itwill/shop/ProductService.java
+++ src/com/itwill/shop/ProductService.java
@@ -1,5 +1,8 @@
 package com.itwill.shop;
 
 public class ProductService {
-
+	private MemberDao memberDao;
+	public ProductService() {
+		memberDao=new MemberDao();
+	}
 }
